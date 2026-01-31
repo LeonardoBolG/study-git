737dbbc79ffc2c3baf420bf92afd735f29536074
Leonardo Bolaños <leonardo.bolanos@balloon-group.com>
2026-01-30 19:10:17 -0500

    another commit to try the git stash

--- my-script.js
+++ my-script.js
@@ -5,12 +5,22 @@ console.log('holiii');
 const myFirstVar = 123;
 console.log('myFirstVar:', myFirstVar);
 
-
 const a = 34;
 
 const b = "hay dude";
 
-let c = a+b;
+let c = a + b;
 
 console.log(c);
 
+console.log('otro cambio');
+
+console.log('último cambio antes de push');
+
+console.log('cambio después del push');
+
+console.log('cambio en la rama feature/nueva-rama');
+
+console.log('otro cambio en la rama feature/nueva-rama');
+
+console.log('cambio en la rama main después de crear feature/nueva-rama');
